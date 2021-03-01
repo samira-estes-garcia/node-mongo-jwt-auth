772fe4be8f58d32b8aa799e195a39948b362de53
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2021-03-01 12:41:35 -0600

    added notes and index.js

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -19,3 +19,10 @@ module.exports = User;
 
 //these mongoose models represent users & roles collections in MongoDB database
 //User object will have a roles array that contians ids in roles collection as reference
+//dont need to write crud functions bc mongo supports all of them! 
+//create new user: object.save();
+//find a user by id: User.findById(id);
+//find a user by email: User.findOne({ email: ... });
+//find a user by username: User.findOne({ username:...});
+//find all ROles which name in given roles array: Role.find({ name: { $in: roles }});
+//these functions will be used in our Controllers and Middlewares
