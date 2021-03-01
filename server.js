763613a7b04d2cd433e84de4f2522d1cdeb91a73
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2021-03-01 12:57:00 -0600

    added db.mongoose and initial function

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ app.use(bodyParser.json());
 //parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//add the following code to open Mongoose connection to MongoDB database;
+const db = require('./app/models')
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connected to MongoDB!");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
 //simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Samira's auth app"});
@@ -26,6 +44,46 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+//initial function
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                
+                console.log("added 'user' to roles collection")
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
+
+
+
 
 //import express, body-parser, and cors modules:
 //-express is for building REST apis
